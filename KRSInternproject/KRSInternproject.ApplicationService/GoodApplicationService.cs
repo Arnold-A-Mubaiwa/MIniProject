e63cecd4c8a6932c5b04050e374865805b22e274
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2023-01-25 21:43:37 +0200

    Passing Test

--- KRSInternproject/KRSInternproject.ApplicationService/GoodApplicationService.cs
+++ KRSInternproject/KRSInternproject.ApplicationService/GoodApplicationService.cs
@@ -4,9 +4,20 @@ namespace KRSInternproject.ApplicationService
 {
   public class GoodApplicationService
   {
+    private readonly GoodValidator _validate = new();
+    List<Good> goods= new List<Good>();
     public bool AddGood(Good good)
     {
-      return false;
+      if (!ValidateGood(good)) return false;
+        
+      goods.Add(good);
+      return true;
+    }
+
+    public bool ValidateGood(Good good)
+    {
+      var valid = _validate.Validate(good);
+      return valid.IsValid;
     }
   }
 }
