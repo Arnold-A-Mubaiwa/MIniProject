9f44496a6e0373ec7c31b06a9c94da5a358c7041
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2023-01-27 08:59:02 +0200

    Items Scarfolding

---
+++ KRSInternproject/KRSInternproject.Domain/ItemValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KRSInternproject.Domain
+{
+  public class ItemValidator: AbstractValidator<Item>
+  {
+    public ItemValidator()
+    {
+      RuleFor(i => i.Price).NotEmpty().GreaterThan(0);
+      RuleFor(i => i.Quantity).NotEmpty().GreaterThan(0);
+      RuleFor(i => i.good).NotEmpty();
+    }
+  }
+}
