145aa352b66c8b2b54126e573e6eedf5cac94ef7
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2023-01-27 09:55:58 +0200

    Purchase Scarfolding complete

---
+++ KRSInternproject/KRSInternproject.Domain/PurchaseOrder.cs
@@ -0,0 +1,20 @@
+﻿
+namespace KRSInternproject.Domain
+{
+  public class PurchaseOrder
+  {
+    public int Number { get; set; }
+    public DateTime OrderDate { get; init; }
+    public Supplier supplier { get; set; }
+    public DateTime EstimatedDateOfDelivery => OrderDate.AddDays(supplier.LeadTime);
+    private List<Item> _items { get; set; } = new();
+    public IEnumerable<Item> Items  => _items;
+
+    public PurchaseOrder(Supplier supplier, List<Item> items)
+    {
+      _items = items;
+      OrderDate = DateTime.Now;
+      this.supplier = supplier;
+    }
+  }
+}
---
+++ KRSInternproject/KRSInternproject.Domain/PurchaseOrderValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace KRSInternproject.Domain
+{
+  public class PurchaseOrderValidator: AbstractValidator<PurchaseOrder>
+  {
+    public PurchaseOrderValidator()
+    {
+      RuleFor(p => p.Number).NotEmpty().GreaterThan(0);
+      RuleFor(p => p.supplier).SetValidator(new SupplierValidator());
+      RuleForEach(p => p.Items).SetValidator(new ItemValidator());
+      RuleFor(p => p.Items).NotEmpty();
+    }
+  }
+}
